22c1f3608864257fd7de7a76d53a560a7ef8c49f
“Okunade <bidemiokunade@gmail.com>
2023-04-17 09:04:24 -0700

    Advanced tasks solved

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const dictKeys = Object.keys(dict);
+const values = Object.values(dict);
+let matched;
+const resultDict = {};
+// loop over the values
+for (let i = 0; i < values.length; i++) {
+  resultDict[JSON.stringify(values[i])] = [];
+  matched = dictKeys.filter(key => dict[key] === values[i]);
+  matched.forEach(item => result[JSON.stringify(values[i])].push(item));
+}
+console.log(resultDict);
