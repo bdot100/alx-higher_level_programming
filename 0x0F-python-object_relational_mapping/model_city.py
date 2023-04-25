1fbc369f08e1fe545b73b123ca75178e0ed179df
“Okunade <bidemiokunade@gmail.com>
2023-04-25 06:16:53 -0700

    City Model Created

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
+
 class City(Base):
     """
     Tis class defines each city
