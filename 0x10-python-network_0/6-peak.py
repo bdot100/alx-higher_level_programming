4f114eb0918965dfb11ae5d115b02e0f6d727a15
“Okunade <bidemiokunade@gmail.com>
2023-05-05 12:27:51 -0700

    Requst Body size done

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script find the peak from an unsorted integer """
+
+def find_peak(list_of_integers):
+    """
+    Args: list_of_integers(int): list of integers to find the peak from
+    Returns: Peak from the list or None
+    """
+
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return nums[left]
