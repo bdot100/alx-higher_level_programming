3eb5bb11c7c1f54583c7f7c2677d9c2c70bb5ff6
Bidemi Okunade <bidemiokunade@gmail.com>
2023-05-29 14:57:25 -0700

    Tasks done

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// This script gets the contents of a webpage and stores it in a file
+const fs = require('fs');
+const request = require('request');
+const args = process.argv.slice(2);
+
+request.get(args[0], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(args[1], body, function (err, res) {
+      if (err) console.log('error', err);
+    });
+  }
+});
